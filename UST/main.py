a80238cbd157c0c0e8cd2958376f03a406807508
EbrahimGhaleb <ebrahimghaleb777@gmail.com>
2024-10-01 22:43:55 +0300

    Add files via upload

---
+++ UST/main.py
@@ -0,0 +1,14 @@
+
+def eeb(x):
+    eeb = int (input("ادخل العدد: "))
+
+    if eeb % 2==0:
+          
+          print("نعم")
+
+    else:
+        
+        print('لا'): # type: ignore
+        
+a=int (input("ادخل الرقم : "))
+eeb(a)
